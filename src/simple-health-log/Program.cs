2c3c62c0c1aa41994943adb04a87bc9919b462f8
jbarszczewski <jakub.barszczewski@outlook.com>
2020-06-14 00:24:27 +0100

    added sln file

--- src/simple-health-log/Program.cs
+++ src/simple-health-log/Program.cs
@@ -10,16 +10,16 @@ using Microsoft.Extensions.Logging;
 
 namespace simple_health_log
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
