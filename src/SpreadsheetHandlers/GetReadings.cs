741e0d2342e76ab29154564be99b1b296e38f616
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2020-07-13 14:45:32 +0100

    fixed dotnet version in workflows

--- src/SpreadsheetHandlers/GetReadings.cs
+++ src/SpreadsheetHandlers/GetReadings.cs
@@ -6,21 +6,16 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace SpreadsheetHandlers {
 	public static class GetReadings {
 		[FunctionName("GetReadings")]
 		public static async Task<IActionResult> Run(
-			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+			[HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
 			ILogger log) {
 			log.LogInformation("C# HTTP trigger function processed a request.");
 
 			string name = req.Query["name"];
-
-			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			dynamic data = JsonConvert.DeserializeObject(requestBody);
-			name ??= data?.name;
 
 			return name != null
 				? (ActionResult)new OkObjectResult($"Hello, {name}")
