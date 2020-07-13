ee9ef406ca33900fdf0f7b58fe2ce4f20ac35595
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2020-07-13 20:45:13 +0100

    added PostReading function

---
+++ src/SpreadsheetHandlers/PostReading.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SpreadsheetHandlers {
+	public static class PostReading {
+		[FunctionName("PostReading")]
+		public static async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log) {
+			return new NoContentResult();
+		}
+	}
+}
